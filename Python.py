35da46bd4dbe98c3c79ade0cafe47e8f9324d2b1
galien1 <galienshadowstriker@gmail.com>
2017-07-07 12:47:59 -0400

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,35 @@
+#This below is a variable set to the value of 3
+
+my_int = 3
+
+#Variables store data like numbers below this prints the number stored in the variable my_int
+
+print my_int
+
+ def spam():
+    Chikin = 12
+    return Chikin
+        
+print spam()
+
+"""This is
+A multi-line
+Comment
+"""
+
+#you can do addition and subtraction etc like below:
+count_to = 30 + 50
+
+print count_to
+#The answer would be 80
+
+#Modulo
+spam = 3%2
+
+print spam
+#The answer would be 1 because 2 can only go into 3 once evenly
+
+#Python Syntax
+monty = True
+python = 1.234
+monty_python = python ** 2
